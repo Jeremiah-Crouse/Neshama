de41254a8c423f59d2d623c04c753252bcd0c3d9
Jeremiah Crouse <jeremiah.crouse@outlook.com>
2025-06-14 19:06:53 -0500

    Update App.js

--- App.js
+++ App.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 3000;
 app.get('/', (_, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Web server running on port ${PORT}`);
 });
 
